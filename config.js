90bc43f9551994ee3bcf0660f49e78fbda1324f6
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-05-01 15:41:52 -0300

    primer deploy

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  mongoUrl:
+    'mongodb+srv://prodemundial:apififacom@prode.x70cn.mongodb.net/ProdeDB?retryWrites=true&w=majority',
+  sessionSecret: 'apififacom',
+  googleClientId:
+    '468076309040-gaddvkpp6tj8fpm5utn6e3fbbrj0jel2.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  clientUrl: 'http://localhost:3000',
+  emailAccount: 'MundialProde@outlook.com',
+  emailPassword: 'api.fifa.com'
+};
+
+export default config;
