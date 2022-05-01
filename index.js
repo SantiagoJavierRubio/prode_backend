85d2c8e93d5d4a3b415bf3bdd561dd993ae20b6e
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-05-01 16:28:41 -0300

    primer deploy

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      secure: false,
+      secure: true,
       sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 24 * 30
     }
