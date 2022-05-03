d159b602a0a5e128ecc41923b814c2f02d93a970
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-05-03 18:56:19 -0300

    sameSite

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       secure: true,
-      sameSite: 'none',
+      sameSite: 'None',
       httpOnly: false,
       maxAge: 1000 * 60 * 60 * 24 * 30
     }
