12e43db69c13e2d56352ba0ec9c5920dfafd1959
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-08-30 23:06:57 -0300

    next matches func

--- controllers/fifa.js
+++ controllers/fifa.js
@@ -7,48 +7,69 @@ import errorHandler from '../Errors/errorHandler.js';
 const fifa = new FifaRepository();
 
 export const getLocaleLanguage = (req, res, next) => {
-    const lang = i18n.getLocale(req)
-    res.locals.lang = lang
-    next()
-}
+  const lang = i18n.getLocale(req);
+  res.locals.lang = lang;
+  next();
+};
 
 export const getFixture = async (req, res, next) => {
-    try {
-        const payload = {}
-        const groupId = req.query.groupId || null;
-        const stageId = req.query.stageId || null;
-        if(groupId) {
-            const groupCode = getGroupCode(groupId)
-            payload.fixture = await fifa.getOneGroup(groupCode, res.locals.lang)
-        }
-        else if (stageId) {
-            const stageCode = getStageCode(stageId)
-            payload.fixture = await fifa.getOneStage(stageCode, res.locals.lang)
-        } 
-        else payload.fixture = await fifa.getAllStages(res.locals.lang)
-        res.json(payload)
-    } 
-    catch (err) {
-        errorHandler(err, req, res, next)
+  try {
+    const payload = {};
+    const groupId = req.query.groupId || null;
+    const stageId = req.query.stageId || null;
+    if (groupId) {
+      const groupCode = getGroupCode(groupId);
+      payload.fixture = await fifa.getOneGroup(groupCode, res.locals.lang);
     }
-}
+    else if (stageId) {
+      const stageCode = getStageCode(stageId);
+      payload.fixture = await fifa.getOneStage(stageCode, res.locals.lang);
+    }
+    else payload.fixture = await fifa.getAllStages(res.locals.lang);
+    res.json(payload);
+  }
+  catch (err) {
+    errorHandler(err, req, res, next);
+  }
+};
 
 export const getGroups = async (req, res, next) => {
-    try {
-        const payload = { fixture: await fifa.getAllGroups(res.locals.lang) }
-        res.json(payload)
-    }
-    catch(err) {
-        errorHandler(err, req, res, next)
-    }
-}
+  try {
+    const payload = { fixture: await fifa.getAllGroups(res.locals.lang) };
+    res.json(payload);
+  }
+  catch (err) {
+    errorHandler(err, req, res, next);
+  }
+};
 
 export const getFixtureStatus = async (req, res, next) => {
-    try {
-        const payload = await checkFixtureStatus();
-        res.json(payload)
-    }
-    catch(err) {
-        errorHandler(err, req, res, next)
-    }
-}
+  try {
+    const payload = await checkFixtureStatus();
+    res.json(payload);
+  }
+  catch (err) {
+    errorHandler(err, req, res, next);
+  }
+};
+
+export const getNextMatches = async (req, res, next) => {
+  try {
+    const quantity = parseInt(req.query.cant) || 5;
+    const allMatches = await fifa.getAllMatches();
+    const now = Date.now();
+    const nextMatches = allMatches
+      .filter(
+        (match) =>
+          match?.home?.name &&
+          match?.away?.name &&
+          now < Date.parse(match?.date)
+      )
+      .sort((a, b) => a.date > b.date)
+      .slice(0, quantity);
+    res.json(nextMatches);
+  }
+  catch (err) {
+    errorHandler(err, req, res, next);
+  }
+};
--- routes/fifa.js
+++ routes/fifa.js
@@ -1,14 +1,21 @@
-import { Router } from 'express'
-import { getFixture, getGroups, getLocaleLanguage, getFixtureStatus } from '../controllers/fifa.js'
+import { Router } from 'express';
+import {
+  getFixture,
+  getGroups,
+  getLocaleLanguage,
+  getFixtureStatus,
+  getNextMatches,
+} from '../controllers/fifa.js';
 
-const router = Router()
+const router = Router();
 
-router.use(getLocaleLanguage)
+router.use(getLocaleLanguage);
 router.get('/', (req, res) => {
-    res.send('Welcome to the API!');
-})
-router.get('/fixture', getFixture)
-router.get('/fixture/groups', getGroups)
-router.get('/fixture-status', getFixtureStatus)
+  res.send('Welcome to the API!');
+});
+router.get('/fixture', getFixture);
+router.get('/fixture/groups', getGroups);
+router.get('/fixture-status', getFixtureStatus);
+router.get('/next-matches', getNextMatches);
 
-export default router
+export default router;
